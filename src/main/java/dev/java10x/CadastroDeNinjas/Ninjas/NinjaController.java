1f4a3ad73ab8435310c6403aba8d01e6eaf417b3
João Victor <joaovictorcarvalhosilva29@gmail.com>
2026-01-13 20:03:38 -0300

    feat: includes the ninja registration route using POST METHOD, initialize POSTMAN

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,16 +1,52 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
 
-    @GetMapping("/boasVindas")
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+
+    //GET  -> Mostrar todos os Ninjas
+    @GetMapping("/listar")
+    public List<NinjaModel> listarNinjas() {
+        return ninjaService.listarNinjas();
+    }
+
+    //GET  -> Mostrar Ninja por ID
+    @GetMapping("/listar/{id}")
+    public NinjaModel mostrarNinjasPorId(@PathVariable Long id) {
+        return ninjaService.listarNinjaPorId(id);
+    }
+
+    //POST  -> Adicionar ninja
+    @PostMapping("/criar")
+    public NinjaModel criarNinja(@RequestBody NinjaModel ninja){
+        return ninjaService.criarNinja(ninja);
+    }
+
+    //PUT  -> Alterar dados dos ninjas
+    @PutMapping("/atualizar/{id}")
+    public String atualizarNinja() {
+        return "Ninja atualizado!";
+    }
+
+    //DELETE  -> Deletar Ninja
+    @DeleteMapping("/deletar/{id}")
+    public String deletarNinja(){
+        return "Ninja deletado!";
+    }
+
+    @GetMapping("/status")
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota";
     }
-
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -3,6 +3,7 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class NinjaService {
@@ -16,5 +17,16 @@ public class NinjaService {
     //Listar todos os ninjas
     public List<NinjaModel> listarNinjas() {
         return ninjaRepository.findAll();
+    }
+
+    //Listar ninja por ID
+    public NinjaModel listarNinjaPorId(Long id){
+        Optional<NinjaModel> ninja = ninjaRepository.findById(id);
+        return ninja.orElse(null);
+    }
+
+    //Criar novo ninja
+    public NinjaModel criarNinja(NinjaModel ninja){
+        return ninjaRepository.save(ninja);
     }
 }
